ad2433ea901a4eec4f5326d910afea0686a7306e
CYZice <yunzechencn@gmail.com>
2026-05-30 13:52:21 +0800

    feat: 添加 CLICommanderSettingTab 和 BUILTIN_PRESETS
    - ui/setting-tab/CLICommanderSettingTab.js
    - shared/constants/presets.js

---
+++ src/shared/constants/presets.js
@@ -0,0 +1,47 @@
+const BUILTIN_PRESETS = [
+  {
+    id: "p-append",
+    name: "快速记录想法",
+    desc: "向今日日记追加记录",
+    commandId: "daily:append",
+    defaultParams: { open: false },
+    quickInput: {
+      param: "content",
+      placeholder: "输入想法（Enter执行/Shift+Enter换行）...",
+    },
+  },
+  {
+    id: "p-tasks",
+    name: "今日待办",
+    desc: "今日日记未完成任务",
+    commandId: "tasks",
+    defaultParams: { daily: true, todo: true },
+    quickInput: null,
+  },
+  {
+    id: "p-unresolved",
+    name: "查找断链",
+    desc: "未解析链接",
+    commandId: "unresolved",
+    defaultParams: { verbose: true },
+    quickInput: null,
+  },
+  {
+    id: "p-alltasks",
+    name: "全部待办",
+    desc: "所有未完成任务",
+    commandId: "tasks",
+    defaultParams: { todo: true, verbose: true },
+    quickInput: null,
+  },
+  {
+    id: "p-tpl",
+    name: "从模板创建",
+    desc: "使用模板创建笔记",
+    commandId: "create",
+    defaultParams: { open: true },
+    quickInput: { param: "template", placeholder: "模板名..." },
+  },
+];
+
+module.exports = { BUILTIN_PRESETS };
